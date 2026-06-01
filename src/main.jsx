bb9b5e05802b08b622e2c316cdb73d3a6fed4b9c
sadaf953 <sadafmahvishedu@gmail.com>
2026-06-02 03:35:43 +0530

    initial solar crm setup2

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { HashRouter } from 'react-router-dom' // Ensure this is imported
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>,
 )
-
-import { HashRouter } from 'react-router-dom'
-// ...
-<HashRouter>
-  <App />
-</HashRouter>
